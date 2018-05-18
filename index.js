3c2d1d048941f729c1bb7fb55c04bba170cadfa9
Trevor Russell <truss.42@gmail.com>
2018-05-18 20:06:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function nestedTarget() {
   return(document.querySelector('#nested .target'))
 }
 function increaseRankBy(n) {
-  var list = document.querySelectorAll('ul.ranked-list le')
+  var list = document.querySelectorAll('.ranked-list li')
   for(var i = 0; i < list.length; i++){
     list[i].innerHTML = parseInt(list[i].innerHTML) + n;
   }
