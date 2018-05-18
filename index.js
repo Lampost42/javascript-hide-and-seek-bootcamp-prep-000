3f88d719eafbe8e350a6c04e2ae3256f49463286
Trevor Russell <truss.42@gmail.com>
2018-05-18 20:06:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ function nestedTarget() {
   return(document.querySelector('#nested .target'))
 }
 function increaseRankBy(n) {
-  var list = document.querySelectorAll('.ranked-list')
+  var list = document.querySelectorAll('.ranked-list le')
   for(var i = 0; i < list.length; i++){
-    list[i] = document.querySelectorAll('li')
-  for(var a = 0; a < list[i].length; a++){
     list[i][a].innerHTML = parseInt(list[i][a].innerHTML) + n;
-  }
   }
 }
